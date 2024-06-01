7bb8ff0816764898fa840d648c3a637c63ee20c1
Aleu79 <aleeaguero79@gmail.com>
2024-06-01 15:48:21 -0300

    second commit:test

--- q
+++
@@ -1,5 +0,0 @@
-user.email=Aleu79
-core.repositoryformatversion=0
-core.filemode=true
-core.bare=false
-core.logallrefupdates=true
--- task/views.py
+++ task/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpRequest
 
 
 # Create your views here.
+
 def home(request):
     return render(request, 'home.html',)
 def signup(request):
+    
+    if request.method == 'GET':
+        print('enviando formulario')
+    else:
+        print(request.POST)
+        print('recibiendo datos')
     return render(request, 'signup.html',)
+
+ 
+
+
+ 
