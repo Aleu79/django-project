c97ade412161b14029d8ff3fab79304b9e27cb15
Aleu79 <aleeaguero79@gmail.com>
2024-06-05 17:30:09 -0300

    eleventh commit

--- task/views.py
+++ task/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.db import IntegrityError
 
 
@@ -44,14 +43,12 @@ def signout(request):
 def signin(request):
     if request.method == 'GET':
         return render(request, 'signin.html', {
-            'form': AuthenticationForm
-        })
+            })
     else:
         user = authenticate(
             request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'signin.html', {
-                'form': AuthenticationForm,
                 "error": 'username or password is incorrect'
             })
         else:
